2a888891d8a631600bbce45d17a084a230876acb
dlqkfzlr <hisunwoong@gmail.com>
2021-01-11 18:02:39 +0900

    Add Flow & PagingData for Paging 3.0

--- app/src/main/java/m/woong/giphyapp/data/repos/GiphyRepository.kt
+++ app/src/main/java/m/woong/giphyapp/data/repos/GiphyRepository.kt
@@ -1,10 +1,12 @@
 package m.woong.giphyapp.data.repos
 
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 import m.woong.giphyapp.data.source.remote.model.RemoteSearchGiphyResponse
 import m.woong.giphyapp.data.source.remote.wrapper.ResWrapper
 
 interface GiphyRepository {
 
     suspend fun requestToSearchGiphy(q: String, limit: Int, offset: Int): ResWrapper<RemoteSearchGiphyResponse>
-
+    fun getSearchResultStream(query: String): Flow<PagingData<RemoteSearchGiphyResponse.Data.Images.PreviewGif>>
 }
--- app/src/main/java/m/woong/giphyapp/data/repos/GiphyRepositoryImpl.kt
+++ app/src/main/java/m/woong/giphyapp/data/repos/GiphyRepositoryImpl.kt
@@ -1,7 +1,13 @@
 package m.woong.giphyapp.data.repos
 
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import m.woong.giphyapp.data.source.paging.GiphyPagingSource
 import m.woong.giphyapp.data.source.remote.RemoteDataSourceImpl
-import m.woong.giphyapp.data.source.remote.model.RemoteSearchGiphyResponse
+import m.woong.giphyapp.data.source.remote.model.RemoteSearchGiphyResponse.Data.Images.PreviewGif
 import javax.inject.Inject
 
 class GiphyRepositoryImpl @Inject constructor(
@@ -14,5 +20,20 @@ class GiphyRepositoryImpl @Inject constructor(
         offset: Int
     ) = safeApiCall  {
         remoteDataSource.requestToSearchGiphy(q, limit, offset)
+    }
+
+    override fun getSearchResultStream(query: String): Flow<PagingData<PreviewGif>> {
+        Log.d("GiphyRepositoryImpl", "New query: $query")
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { GiphyPagingSource(remoteDataSource, query) }
+        ).flow
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 50
     }
 }
